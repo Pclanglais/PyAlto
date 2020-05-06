30475cc9694b52c1cb3a643c3532aee64405d6b6
Numapresse <42674826+Numapresse@users.noreply.github.com>
2020-05-06 16:20:57 +0200

    Update PyAlto.py

--- PyAlto.py
+++ PyAlto.py
@@ -2,7 +2,7 @@
 from parse_alto import *
       
 #Try to parse any file
-#parse_alto_file("liberte_1866_07_18_ex1_p2.xml")
+parse_alto_file("liberte_1866_07_18_ex1_p2.xml")
 
 #Optionally you can iterate over a directory:
-parse_alto_directory("alto_dir", "result_dir")
+#parse_alto_directory("alto_dir", "result_dir")
