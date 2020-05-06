5a41d761e9074d372acb36332b25b72ed886f77f
Numapresse <42674826+Numapresse@users.noreply.github.com>
2020-05-06 14:19:52 +0200

    Add files via upload

---
+++ PyAlto.py
@@ -0,0 +1,8 @@
+#Loading the local python scrips
+from parse_alto import *
+      
+#Try to parse any file
+#parse_alto_file("liberte_1866_07_18_ex1_p2.xml")
+
+#Optionally you can iterate over a directory:
+parse_alto_directory("alto_dir", "result_dir")
