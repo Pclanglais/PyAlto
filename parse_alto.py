401b638d76c77a606c9d88c38f6d0b734e5b82fb
Numapresse <42674826+Numapresse@users.noreply.github.com>
2020-05-06 14:18:56 +0200

    Update parse_alto.py

--- parse_alto.py
+++ parse_alto.py
@@ -2,6 +2,7 @@
 from parse_alto_class import *
 from parse_alto_extract import *
 import os
+import glob
 
 
 #A function to write the textual data files into the directory of each issue of a newspaper
@@ -14,7 +15,7 @@ def write_alto_data(final_file_name, end_file, alto_data, zip_file = False):
     os.remove(final_file_name)
 
 #A function to generate all the data files compiling ALTO and Spacy outputs.
-def parse_alto_file(file_name, zip_file = False):
+def parse_alto_file(file_name, result_dir = False, zip_file = False):
   
   alto_data = DataAlto(style = ["\t".join(["work", "date", "issue", "page", "id_style", "size", "font"])], 
           block_metadata = ["\t".join(["work", "date", "issue", "page", "composed_block", "block", "type", "height", "width", "hpos", "vpos", "shape", "id_style_meta", "text"])],
@@ -26,13 +27,20 @@ def parse_alto_file(file_name, zip_file = False):
   alto_data = extract_alto(file_name, alto_data)
 
   #Removing the end file in .xml
-  final_file_name = file_name[:-4]
+  if result_dir is False:
+    final_file_name = file_name[:-4]
+  else:
+    final_file_name = result_dir + "/" + file_name[:-4].split("/")[-1]
       
   write_alto_data(final_file_name, "style", "\n".join(alto_data.style), zip_file)
   write_alto_data(final_file_name, "block_metadata", "\n".join(alto_data.block_metadata), zip_file)
   write_alto_data(final_file_name, "line_metadata", "\n".join(alto_data.line_metadata), zip_file)
   write_alto_data(final_file_name, "ocr_text", "\n".join(alto_data.ocr_text), zip_file)
   write_alto_data(final_file_name, "ngram", "\n".join(alto_data.ngram), zip_file)
-      
 
-parse_alto_file("liberte_1866_07_18_ex1_p2.xml")
+  print(final_file_name + "is done.")
+
+#A function to iterate over a directory.
+def parse_alto_directory(source_dir, result_dir, zip_file = False):
+  for file in glob.glob(source_dir + "/*.xml"):
+    parse_alto_file(file, result_dir = result_dir, zip_file = zip_file)
